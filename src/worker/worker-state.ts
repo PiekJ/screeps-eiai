2a5f9d494e485872c9983c8733d00f6fb91ae0df
PiekJ <jos.piek@kpnmail.nl>
2020-11-19 18:16:10 +0100

    Worker performance improvements. Can now build structures and repair roads.

--- src/worker/worker-state.ts
+++ src/worker/worker-state.ts
@@ -4,6 +4,7 @@ export const WORKER_STATE_HARVEST: WORKER_STATE_HARVEST = 2;
 export const WORKER_STATE_START_PROCESS_ENERGY: WORKER_STATE_START_PROCESS_ENERGY = 3;
 export const WORKER_STATE_TRANSFER: WORKER_STATE_TRANSFER = 4;
 export const WORKER_STATE_BUILD: WORKER_STATE_BUILD = 5;
+export const WORKER_STATE_REPAIR: WORKER_STATE_REPAIR = 6;
 
 type WORKER_STATE_UNKNOWN = 0;
 type WORKER_STATE_START_HARVEST = 1;
@@ -11,6 +12,7 @@ type WORKER_STATE_HARVEST = 2;
 type WORKER_STATE_START_PROCESS_ENERGY = 3;
 type WORKER_STATE_TRANSFER = 4;
 type WORKER_STATE_BUILD = 5;
+type WORKER_STATE_REPAIR = 6;
 
 type WorkerStateConstant =
     | WORKER_STATE_UNKNOWN
@@ -18,16 +20,19 @@ type WorkerStateConstant =
     | WORKER_STATE_HARVEST
     | WORKER_STATE_START_PROCESS_ENERGY
     | WORKER_STATE_TRANSFER
-    | WORKER_STATE_BUILD;
+    | WORKER_STATE_BUILD
+    | WORKER_STATE_REPAIR;
 
 interface StateMemoryTypes {
     [key: number]:
         | HarvestStateMemory
         | TransferStateMemory
-        | BuildStateMemory;
+        | BuildStateMemory
+        | RepairStateMemory;
     [WORKER_STATE_HARVEST]: HarvestStateMemory;
     [WORKER_STATE_TRANSFER]: TransferStateMemory;
     [WORKER_STATE_BUILD]: BuildStateMemory;
+    [WORKER_STATE_REPAIR]: RepairStateMemory;
 }
 
 interface HarvestStateMemory {
@@ -40,6 +45,10 @@ interface TransferStateMemory {
 
 interface BuildStateMemory {
     constructionSiteId: Id<ConstructionSite>;
+}
+
+interface RepairStateMemory {
+    structureId: Id<Structure>;
 }
 
 export function getStateMemory<K extends WorkerStateConstant>(creep: Creep, state: K): StateMemoryTypes[K] {
--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,4 +1,4 @@
-import { WORKER_STATE_UNKNOWN, setState, WORKER_STATE_HARVEST, WORKER_STATE_START_HARVEST, WORKER_STATE_START_PROCESS_ENERGY, getStateMemory, WORKER_STATE_TRANSFER } from "./worker-state";
+import { WORKER_STATE_UNKNOWN, setState, WORKER_STATE_HARVEST, WORKER_STATE_START_HARVEST, WORKER_STATE_START_PROCESS_ENERGY, getStateMemory, WORKER_STATE_TRANSFER, WORKER_STATE_BUILD, WORKER_STATE_REPAIR } from "./worker-state";
 
 export function runWorker(creep: Creep) {
     if (!creep.memory.state) {
@@ -11,15 +11,17 @@ export function runWorker(creep: Creep) {
 
             if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
                 setState(creep, WORKER_STATE_START_HARVEST);
+                runWorker(creep);
             }
             break;
 
         case WORKER_STATE_START_HARVEST:
             console.log(creep.name, 'STATE: START HARVEST');
 
-            let sources = creep.room.find(FIND_SOURCES_ACTIVE);
+            const sources = creep.room.find(FIND_SOURCES_ACTIVE);
 
             setState(creep, WORKER_STATE_HARVEST, { sourceId:  sources[0].id });
+            runWorker(creep);
             break;
 
         case WORKER_STATE_HARVEST:
@@ -27,14 +29,15 @@ export function runWorker(creep: Creep) {
 
             if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
                 setState(creep, WORKER_STATE_START_PROCESS_ENERGY);
+                runWorker(creep);
                 break;
             }
 
-            let harvestStateMemory = getStateMemory(creep, WORKER_STATE_HARVEST);
+            const harvestStateMemory = getStateMemory(creep, WORKER_STATE_HARVEST);
 
-            let source = Game.getObjectById(harvestStateMemory.sourceId)!;
+            const source = Game.getObjectById(harvestStateMemory.sourceId)!;
 
-            if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
             break;
@@ -44,40 +47,107 @@ export function runWorker(creep: Creep) {
 
             if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
                 setState(creep, WORKER_STATE_START_HARVEST);
+                runWorker(creep);
                 break;
             }
 
-            let transferStateMemory = getStateMemory(creep, WORKER_STATE_TRANSFER);
+            const transferStateMemory = getStateMemory(creep, WORKER_STATE_TRANSFER);
 
-            let structure = Game.getObjectById(transferStateMemory.structureId)!;
+            const transferToStructure = Game.getObjectById(transferStateMemory.structureId)!;
 
-            switch (creep.transfer(structure, RESOURCE_ENERGY)) {
+            switch (creep.transfer(transferToStructure, RESOURCE_ENERGY)) {
                 case ERR_NOT_IN_RANGE:
-                    creep.moveTo(structure, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    creep.moveTo(transferToStructure, {visualizePathStyle: {stroke: '#ffaa00'}});
                     break;
 
                 case ERR_FULL:
                     setState(creep, WORKER_STATE_START_PROCESS_ENERGY);
+                    runWorker(creep);
                     break;
+            }
+            break;
+
+        case WORKER_STATE_BUILD:
+            console.log(creep.name, 'STATE: BUILD');
+
+            if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
+                setState(creep, WORKER_STATE_START_HARVEST);
+                runWorker(creep);
+                break;
+            }
+
+            const buildStateMemory = getStateMemory(creep, WORKER_STATE_BUILD);
+
+            const constructionSiteToBuild = Game.getObjectById(buildStateMemory.constructionSiteId);
+
+            if (constructionSiteToBuild) {
+                if (creep.build(constructionSiteToBuild!) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(constructionSiteToBuild!, {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            else {
+                setState(creep, WORKER_STATE_START_PROCESS_ENERGY);
+                runWorker(creep);
+            }
+            break;
+
+        case WORKER_STATE_REPAIR:
+            console.log(creep.name, 'STATE: REPAIR');
+
+            if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
+                setState(creep, WORKER_STATE_START_HARVEST);
+                runWorker(creep);
+                break;
+            }
+
+            const repairStateMemory = getStateMemory(creep, WORKER_STATE_REPAIR);
+
+            const structureToRepair = Game.getObjectById(repairStateMemory.structureId)!;
+
+            if (structureToRepair.hits >= structureToRepair.hitsMax) {
+                setState(creep, WORKER_STATE_START_PROCESS_ENERGY);
+                runWorker(creep);
+                break;
+            }
+
+            if (creep.repair(structureToRepair) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(structureToRepair, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
             break;
 
         case WORKER_STATE_START_PROCESS_ENERGY:
             console.log(creep.name, 'STATE: START PROCESS ENERGY');
 
-            let possibleTargets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
+            const possibleConstructionSites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+
+            if (possibleConstructionSites.length > 0) {
+                setState(creep, WORKER_STATE_BUILD, { constructionSiteId: possibleConstructionSites[0].id });
+                runWorker(creep);
+                break;
+            }
+
+            const possibleStructuresToRepair = creep.room.find(FIND_STRUCTURES, {
+                filter: structure => structure.structureType === STRUCTURE_ROAD && structure.hits / structure.hitsMax < 0.5
             });
 
-            if (possibleTargets.length > 0) {
-                setState(creep, WORKER_STATE_TRANSFER, { structureId: possibleTargets[0].id });
+            if (possibleStructuresToRepair.length > 0) {
+                setState(creep, WORKER_STATE_REPAIR, { structureId: possibleStructuresToRepair[0].id });
+                runWorker(creep);
+                break;
+            }
+
+            const possibleStructuresToTransfer = creep.room.find(FIND_MY_STRUCTURES, {
+                filter: structure => (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
+                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            });
+
+            if (possibleStructuresToTransfer.length > 0) {
+                setState(creep, WORKER_STATE_TRANSFER, { structureId: possibleStructuresToTransfer[0].id });
             }
             else {
                 setState(creep, WORKER_STATE_TRANSFER, { structureId: creep.room.controller!.id });
             }
+            runWorker(creep);
             break;
     }
 }
